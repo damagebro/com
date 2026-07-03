887775ff4ebbb619cd6ac534b478461fe8d33415
damagebro <damagebro@163.com>
2026-07-03 21:06:15 +0800

    x

--- impl_template/impl.f
+++ impl_template/impl.f
@@ -1,11 +1,8 @@
 // $IMPL_PATH/define/impl_define.sv
 // $IMPL_PATH/define/impl_define_sim.sv
 
-$IMPL_PATH/dw/com_ecc_secded.sv
--f $IMPL_PATH/memory/memory.f
 $IMPL_PATH/stdcell/com_cdc_sig.sv
 $IMPL_PATH/stdcell/com_clk_gate.sv
 
-//+incdir+/nfs54/EDA/synopsys/syn/O-2018.06/dw/sim_ver
-///nfs54/EDA/synopsys/syn/O-2018.06/dw/sim_ver/DW_exp2.v
-///nfs54/EDA/synopsys/syn/O-2018.06/dw/sim_ver/DW_fp_exp.v
+$IMPL_PATH/dw/com_ecc_secded.sv
+-f $IMPL_PATH/memory/memory.f
